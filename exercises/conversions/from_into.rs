6faa0300b7df30213983de17d126a759de202235
Louis Wong <louis.d.wong@oracle.com>
2021-01-18 06:08:21 -0800

    More learnings

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,22 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default()
+        }
+        let split: Vec<&str> = s.split(',').collect();
+        if split.len() != 2 {
+            return Person::default()
+        }
+        if split[0].len() == 0 {
+            return Person::default()
+        }
+        if let Ok(x) = split[1].parse::<usize>() {
+            return Person {name: String::from(split[0]), age: x}
+        }
+        Person::default()
     }
 }
 
